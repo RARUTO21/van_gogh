cd2f47dbc0f31dcaa0bfd785e4eebdb497d11b14
Anthony <Anthonylle@hotmail.com>
2017-02-22 16:52:23 -0600

    Pruebas

--- van_gogh.py
+++ van_gogh.py
@@ -1,4 +1,34 @@
 from PIL import Image
+import numpy as np
 
-string = "Hola mundo"
-print (string)
+poblacionAnterior = []
+poblacionActual = []
+
+
+im = Image.open("b.jpg")
+#im.show()
+
+arreglo = np.array(im)
+
+arr = im.load()
+
+print(arreglo[20,30])
+
+#print(arr.getpixel(20,30))
+
+
+
+
+
+def generarPoblacionInicial(size):
+    cols = []
+    rows = []
+
+    
+
+    
+from math import*
+ 
+def euclidean_distance(x,y):
+ 
+    return sqrt(sum(pow(a-b,2) for a, b in zip(x, y)))
