1c06ed47a1da5d89de92e55b34770a07df78ece9
Geovanny-Burgos-Retana <geoburgosretana@gmail.com>
2017-02-22 23:17:16 -0600

    Poblacion Inicial

--- van_gogh.py
+++ van_gogh.py
@@ -1,12 +1,14 @@
 from PIL import Image
 import numpy as np
+import random
+from math import*
 
 poblacionAnterior = []
 poblacionActual = []
 
 
-im = Image.open("a2.png")
-im2 = Image.open("a.png")
+im = Image.open("a2.png").convert("RGB")
+im2 = Image.open("a.png").convert("RGB")
 #im.show()
 
 arreglo = np.array(im)
@@ -18,19 +20,25 @@ arr = im.load()
 
 #print(arr.getpixel(20,30))
 
-
-
-
-
 def generarPoblacionInicial(size):
     cols = []
     rows = []
 
+def GenerarPoblacionInicial():
+    global poblacionAnterior
+    generacion = []
+    for i in range(0,len(arreglo)):
+        generacion.append([])
+        for j in range(0,len(arreglo[i])):
+            generacion[i].append([])
+            for k in range(0,3):
+                generacion[i][j].append([])
+                generacion[i][j][k] = random.randint(0,32)
+    print(generacion)
+    poblacionAnterior = generacion
     
-
-    
-from math import*
  
 def euclidean_distance(x,y):
- 
     return sqrt(sum(pow(a-b,2) for a, b in zip(x, y)))
+
+
